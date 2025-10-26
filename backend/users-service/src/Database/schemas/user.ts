d05113de6f740eba6663bb1f45f8f8aba9061227
Alexiiis <okalexiiis@gmail.com>
2025-10-26 16:11:33 -0700

    Schema updated

--- backend/users-service/src/Database/schemas/user.ts
+++ backend/users-service/src/Database/schemas/user.ts
@@ -1,8 +1,16 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, varchar, pgEnum, date } from "drizzle-orm/pg-core";
+import { Goals } from "../../Interfaces/Goals";
+
+export const goalsEnum = pgEnum("goals", Goals);
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+  name: varchar({ length: 255 }).notNull(),
+  password_hash: varchar({ length: 255 }).notNull(),
+  age: integer().notNull(),
+  height: integer().notNull(),
+  weight: integer().notNull(),
+  goal: goalsEnum().notNull(),
+  created_at: date().notNull().defaultNow()
 });
