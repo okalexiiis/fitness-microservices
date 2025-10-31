d95ea19176609dd2050e3b4b24055f84e43e4ac7
Alexiiis <okalexiiis@gmail.com>
2025-10-31 16:02:54 -0700

    Fix: Return id from an array without index + error on the URL

--- backend/nutrition-service/src/api/index.ts
+++ backend/nutrition-service/src/api/index.ts
@@ -1,10 +1,11 @@
 import { ApiResponse } from "../interfaces/api-types";
 import { User } from "../interfaces/users";
 
-export const USERS_API = "http://users-service:4000";
+export const USERS_API = "http://gateway:4000/api/users";
 
-export const GetUserByID = async (id: number): Promise<number> => {
+export const GetUserByID = async (id: number): Promise<number | null> => {
   const res = await fetch(`${USERS_API}/${id}`);
   const user: ApiResponse<User> = await res.json();
-  return user.data.id
+  console.log("User Data: ", user.data)
+  return user.data[0].id || null
 };
