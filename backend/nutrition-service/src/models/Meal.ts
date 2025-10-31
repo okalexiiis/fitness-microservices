addf94f7f0efe5eb9292d0435ec94f63fcbe84dd
Alexiiis <okalexiiis@gmail.com>
2025-10-31 12:28:14 -0700

    Drizzle Sucks so i need to update types

--- backend/nutrition-service/src/models/Meal.ts
+++ backend/nutrition-service/src/models/Meal.ts
@@ -1,10 +1,17 @@
-import { MealType } from "../interfaces/meal-type"
+import { MealType } from "../interfaces/meal-type";
 
 export interface Meal {
-    id: number
-    food_id: number
-    user_id: number
-    date: Date
-    meal_type: MealType
-    quantity: number
+  id: number;
+  food_id: number;
+  user_id: number;
+  date: string | Date;
+  meal_type: MealType;
+  quantity: number;
 }
+
+export type MealButDateString = Omit<Meal, "date"> & {
+  date: string;
+};
+
+// DTO for creation (without id)
+export interface CreateMealDTO extends Omit<MealButDateString, "id"> {}
