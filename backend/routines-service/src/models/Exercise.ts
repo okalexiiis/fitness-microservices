d2623d2106ff19681c866610e6219435b21272bf
Alexiiis <okalexiiis@gmail.com>
2025-11-01 14:42:46 -0700

    Added Filters and correct spelling

--- backend/routines-service/src/models/Exercise.ts
+++ backend/routines-service/src/models/Exercise.ts
@@ -2,13 +2,17 @@ export interface Exercise {
   id: number;
   name: string;
   description: string;
-  category: string;
+  category: ExerciseCategoryType;
   duration_minutes: number;
   calories_burned: number;
 }
 
 export interface createExerciseDTO extends Omit<Exercise, "id"> {}
-export interface updateExercuseDTO extends Partial<createExerciseDTO> {}
+export interface updateExerciseDTO extends Partial<createExerciseDTO> {}
 
-export const ExerciseCategory = ["cardio", "stregth"] as const
-export type ExerciseCategoryType = typeof ExerciseCategory[number]
+export const ExerciseCategory = ["cardio", "stregth"] as const;
+export type ExerciseCategoryType = (typeof ExerciseCategory)[number];
+
+export type ExerciseFilters = Partial<
+  Pick<Exercise, "id" | "name" | "category">
+>;
