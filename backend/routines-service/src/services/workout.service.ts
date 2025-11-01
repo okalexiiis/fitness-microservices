bf316262c6c8d0e71a4f345cc6cea552a65fbdf3
Alexiiis <okalexiiis@gmail.com>
2025-11-01 14:50:26 -0700

    workout service

---
+++ backend/routines-service/src/services/workout.service.ts
@@ -0,0 +1,68 @@
+import { db, workoutTable } from "../db";
+import { eq, sql } from "drizzle-orm/sql";
+import {
+  createWorkoutDTO,
+  Workout,
+  updateWorkoutDTO,
+  workoutFilters,
+} from "../models/Workout";
+import { applyFilters, sanitizeUpdates } from "../helpers";
+
+export class WorkoutService {
+  private _db = db;
+
+  public async save(raw: createWorkoutDTO): Promise<void> {
+    await this._db.insert(workoutTable).values(raw);
+  }
+
+  public async getAll(
+    page: number = 1,
+    limit: number = 10,
+    filters?: workoutFilters
+  ): Promise<{ total: number; data: Workout[] }> {
+    const offset = (page - 1) * limit;
+
+    let query = this._db.select().from(workoutTable);
+    query = applyFilters({ entity: workoutTable, query, filters });
+
+    const workouts: Workout[] = await query.limit(limit).offset(offset);
+
+    let totalQuery = this._db
+      .select({ total: sql<number>`COUNT(*)` })
+      .from(workoutTable);
+    totalQuery = applyFilters({
+      entity: workoutTable,
+      query: totalQuery,
+      filters,
+    });
+    const totalResult = await totalQuery;
+    const total = totalResult[0].total;
+
+    return { total: total, data: workouts };
+  }
+
+  public async getOneBy(criteria: workoutFilters): Promise<Workout | null> {
+    let query = this._db.select().from(workoutTable);
+    query = applyFilters({ entity: workoutTable, query, filters: criteria });
+    const workout: Workout | undefined = await query
+      .limit(1)
+      .then((res) => res[0]);
+    return workout || null;
+  }
+
+  public async update(id: number, updates: updateWorkoutDTO): Promise<void> {
+    const cleanUpdates = sanitizeUpdates(updates);
+
+    if (!cleanUpdates)
+      throw new Error("No values to update", { cause: { code: 100 } });
+
+    await this._db
+      .update(workoutTable)
+      .set(cleanUpdates)
+      .where(eq(workoutTable.id, id));
+  }
+
+  public async delete(id: number): Promise<void> {
+    await this._db.delete(workoutTable).where(eq(workoutTable.id, id));
+  }
+}
