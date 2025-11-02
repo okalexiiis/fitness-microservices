ddcbf25628ed7533aa1dca995579c0ede44857c2
Alexiiis <okalexiiis@gmail.com>
2025-11-01 21:35:00 -0700

    Cambio de tipado

--- backend/nutrition-service/src/api/index.ts
+++ backend/nutrition-service/src/api/index.ts
@@ -1,10 +1,10 @@
-import { ApiResponse } from "../interfaces/api-types";
+import { ApiResponse, ApiResponsePaginated } from "../interfaces/api-types";
 import { User } from "../interfaces/users";
 
 export const USERS_API = "http://gateway:4000/api/users";
 
 export const GetUserByID = async (id: number): Promise<number | null> => {
   const res = await fetch(`${USERS_API}/${id}`);
-  const user: ApiResponse<User> = await res.json();
+  const user: ApiResponsePaginated<User> = await res.json();
   return user.data[0].id || null;
 };
