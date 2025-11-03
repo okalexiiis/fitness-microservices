3c02f63c10c3f4a3ed12bfa2bc26733a094eb26a
Alexiiis <okalexiiis@gmail.com>
2025-11-03 12:21:04 -0700

    Setting up the project deleting unuseful files, configs, and configuring aliases

--- client-web/vite.config.ts
+++ client-web/vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@shared': path.resolve(__dirname, './src/shared'),
+      '@users': path.resolve(__dirname, './src/users'),
+      '@auth': path.resolve(__dirname, './src/auth'),
+      '@workouts': path.resolve(__dirname, './src/routines/workouts'),
+      '@exercises': path.resolve(__dirname, './src/routines/exercises'),
+      '@foods': path.resolve(__dirname, './src/nutrition/foods'),
+      '@meals': path.resolve(__dirname, './src/nutrition/meals'),
+    },
+  },
 })
